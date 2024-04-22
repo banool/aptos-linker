49f0613490791fa8446e81683b26d38f4bc21232
Daniel Porteous <daniel@dport.me>
2024-04-22 20:23:16 +0100

    Build initial site with just Petra support for now

---
+++ src/pages/GoPage/index.tsx
@@ -0,0 +1,91 @@
+import { CheckIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Container,
+  HStack,
+  Image,
+  Text,
+  VStack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Link, useSearchParams } from "react-router-dom";
+
+type Wallet = {
+  id: string;
+  name: string;
+  iconUrl: string;
+  /** For now we just always set this to true. Later on we can try do detection. */
+  detected: boolean;
+  buildUrl: (url: string) => string;
+};
+
+const wallets: Wallet[] = [
+  {
+    id: "1",
+    name: "Petra",
+    iconUrl:
+      "https://raw.githubusercontent.com/hippospace/aptos-wallet-adapter/main/logos/petra.png",
+    detected: true,
+    buildUrl: (url: string) =>
+      `https://petra.app/explore?link=${encodeURI(url)}`,
+  },
+];
+
+const WalletList = ({ linkUrl }: { linkUrl: string }) => {
+  const bgColor = useColorModeValue("gray.100", "gray.700");
+  const highlightColor = useColorModeValue("gray.100", "gray.700");
+
+  return (
+    <Container>
+      <VStack spacing={4} mt={4}>
+        {wallets.map((wallet) => (
+          <Box
+            w="80%"
+            p={4}
+            bg={wallet.detected ? highlightColor : bgColor}
+            borderRadius="xl"
+            alignItems="center"
+          >
+            <Link key={wallet.id} to={wallet.buildUrl(linkUrl)}>
+              <HStack key={wallet.id} spacing={4}>
+                <Image src={wallet.iconUrl} boxSize="64px" />
+                <Text fontSize="xl" flex="1">
+                  {wallet.name}
+                </Text>
+              </HStack>
+            </Link>
+          </Box>
+        ))}
+      </VStack>
+    </Container>
+  );
+
+  // {wallet.detected && <CheckIcon color="green.500" />}
+};
+
+export const GoPage = () => {
+  // Read from query params.
+  const [searchParams] = useSearchParams();
+
+  const linkUrl = searchParams.get("link");
+
+  if (!linkUrl) {
+    return (
+      <Container>
+        <Text>
+          Please provide a link to a wallet. For example, try{" "}
+          <code>
+            {window.location.href}?link=https://stake.amnis.finance/mint
+          </code>
+          .
+        </Text>
+      </Container>
+    );
+  }
+
+  return (
+    <>
+      <WalletList linkUrl={linkUrl} />
+    </>
+  );
+};
